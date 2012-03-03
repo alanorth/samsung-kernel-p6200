b10c2377a6e10feaef402674aa7a9afa43b92d60
Alan Orth <alan.orth@gmail.com>
2012-03-04 00:25:29 +0300

    Add build script
    The build script sets the proper environment variables required for
    building as well as automates cleaning and copying of both initramfs
    and kernel modules.

---
+++ build-kernel.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function setenv {
+	echo -n "Setting ARM environment..."
+	export ARCH=arm
+	export CROSS_COMPILE="ccache /home/aorth/src/android-ndk-r6/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-"
+	echo " done."
+
+	echo -n "Setting other environment variables..."
+	# the kernel version we're compiling for
+	# ... this corresponds to DXKL3
+	export LOCALVERSION="-P6200DXKL3-CL608366"
+	# the number of CPUs to use when compiling the kernel (auto detect all available)
+	export CPUS=`grep -c processor /proc/cpuinfo`
+	echo " done."
+}
+
+setenv
+
+rm -rf usr/initramfs
+cp -R ../GT-P6200-initramfs/DXKL3 usr/initramfs
+make clean
+schedtool -B -n19 -e make -j${CPUS} modules
+cp $(find . -name "*.ko") usr/initramfs/lib/modules/
+schedtool -B -n19 -e make -j${CPUS}
+
+if [ $$ -eq 0 ]
+then
+	echo "Mmm... smells like bacon!"
+	ls -l arch/arm/boot/zImage
+fi
